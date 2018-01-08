8ccf77da1118977dea3132cf3e7366c85c0eae30
IreneFuji <gigabit001@hotmail.com>
2018-01-08 23:51:21 +0800

    openxml

---
+++ TestConsole/WordXml.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace TestConsole
+{
+	class WordXml
+	{
+		private static string CurrDir = System.Environment.CurrentDirectory;
+		static string outputFile = CurrDir + @"\new.xml";
+
+		static void Main(string[] args) {
+			// 建立 WordprocessingDocument 類別，透過 WordprocessingDocument 類別中的 Create 方法建立 Word 文件
+			using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(outputFile, WordprocessingDocumentType.Document)) {
+				// 建立 MainDocumentPart 類別物件 mainPart，加入主文件部分
+				MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+				// 實例化 Document(w) 部分
+				mainPart.Document = new Document();
+				// 建立 Body 類別物件，於加入 Doucment(w) 中加入 Body 內文
+				Body body = mainPart.Document.AppendChild(new Body());
+				// 建立 Paragraph 類別物件，於 Body 本文中加入段落 Paragraph(p)
+				Paragraph paragraph = body.AppendChild(new Paragraph());
+				// 建立 Run 類別物件，於 段落 Paragraph(p) 中加入文字屬性 Run(r) 範圍
+				Run run = paragraph.AppendChild(new Run());
+				// 在文字屬性 Run(r) 範圍中加入文字內容
+				run.AppendChild(new Text("在 body 本文內容產生 text 文字"));
+			}
+
+			Process.Start(outputFile);
+		}
+
+	}
+}
